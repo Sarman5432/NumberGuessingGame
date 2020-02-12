731985d2d4e781e186115af561e7525e205124ab
Sarman <saulakh@kenna.ca>
2020-02-12 14:26:05 -0500

    completed the basic version

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var gameBox = document.querySelector('#gameBox')
 var textbox = document.querySelector('#textbox')
 var submitBtn = document.querySelector('#submit')
+var prevGuess = document.createElement('p')
+var endResult = document.createElement('p');
+var hint = document.createElement('p');
 
 var firstClick = true;
 var count = 0;
@@ -9,15 +12,19 @@ var guess;
 var answer = parseInt(Math.random()*100);
 
 submitBtn.addEventListener('click', function () {
-    var prevGuess = document.createElement('p')
     if (firstClick) {
         prevGuess.textContent = 'Previous Guesses: '
+        gameBox.append(prevGuess);
         firstClick = false
     }
-    
     guess = textbox.value;
-    gameBox.appendChild(prevGuess);
+    prevGuess.textContent = prevGuess.textContent + ' ' + guess
+    gameBox.append(prevGuess);
     count++;
+
+    textbox.value = ''; //empties the textbox
+
+    compareAnswer();
 
     if(count === 10){
         submitBtn.disabled = true;
@@ -26,5 +33,40 @@ submitBtn.addEventListener('click', function () {
 });
 
 function compareAnswer(){
+    endResult.style.backgroundColor = "red"
     
+    if(count===10){
+        endResult.textContent = '!!!!GAME OVER!!!!'
+        hint.textContent = ''
+    }else{
+        if(guess < answer){
+            endResult.textContent = 'Wrong!'
+            hint.textContent = 'Last guess was too low'
+        }else if(guess > answer){
+            endResult.textContent = 'Wrong!'
+            hint.textContent = 'Last guess was too high'
+        }else if(guess == answer){
+            endResult.textContent = 'Congratulations! You got it right!'
+            /*
+            setStylesOnElement(endResult, {
+                backgroundColor = "green",
+                color: "white"
+            });
+            */
+            endResult.style.backgroundColor = "green"
+            endResult.style.color = "white"
+            
+            hint.textContent = ''
+        }else{
+            endResult.textContent = 'Wrong!'
+            hint.textContent = 'Error: please enter a number'
+        }
+    }
+
+    gameBox.append(endResult);
+    gameBox.append(hint);
+}
+
+const setStylesOnElement = (element, styles) => {
+    Object.assign(element.style, styles)
 }
